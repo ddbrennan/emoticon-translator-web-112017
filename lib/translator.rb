ef3006af6c96b65602db1a9be8f03eb7d7ed21d7
David Brennan <ddbren@gmail.com>
2017-11-07 20:44:21 -0500

    Emoticon translator passes all tests

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,33 @@
-# require modules here
+require "pry"
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  japanese = {}
+  english = {}
+
+  emoticons.each do |meaning, faces|
+      english[faces[0]] = faces[1]
+      japanese[faces[1]] = meaning
+  end
+
+  {"get_emoticon" => english, "get_meaning" => japanese}
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  emo = load_library(path)["get_emoticon"][emoticon]
+  if !emo
+    "Sorry, that emoticon was not found"
+  else
+    emo
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon)
+  emo = load_library(path)["get_meaning"][emoticon]
+  if !emo
+    "Sorry, that emoticon was not found"
+  else
+    emo
+  end
 end
